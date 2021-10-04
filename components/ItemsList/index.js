1295eb06e61162e5e4876c1a33e3d34a856d697f
soroush behmardi <sori.bk@gmail.com>
2021-10-04 15:59:35 -0700

    reformats

--- components/ItemsList/index.js
+++ components/ItemsList/index.js
@@ -1,33 +1,28 @@
-import React from 'react'
+import React from 'react';
 import ListItem from './ListItem';
-import { View, FlatList, StyleSheet } from 'react-native'
+import {View, FlatList, StyleSheet} from 'react-native';
 
 export default function ItemsList({data, onDelete}) {
-    const renderFunction = ({item}) => (
-        <ListItem 
-            style={styles.listItemStyles} 
-            item={item} 
-            onDelete={onDelete}
-        />
-    )
-    
-    return (
-        <View style={styles.container}>
-            <FlatList 
-                data={data}
-                renderItem={renderFunction}
-                keyExtractor={item => String(item.id)}
-            />
-        </View>
-    )
+  const renderFunction = ({item}) => (
+    <ListItem style={styles.listItemStyles} item={item} onDelete={onDelete} />
+  );
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={data}
+        renderItem={renderFunction}
+        keyExtractor={(item) => String(item.id)}
+      />
+    </View>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-        width: '100%'
-    },
-    listItemStyles: {
-        marginVertical: 3
-    }
-})
+  container: {
+    width: '100%',
+  },
+  listItemStyles: {
+    marginVertical: 3,
+  },
+});
