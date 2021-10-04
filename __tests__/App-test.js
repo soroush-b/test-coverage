e3891d7a71ddb6999bd80988474511da7ced84fb
soroush behmardi <sori.bk@gmail.com>
2021-10-03 19:57:45 -0700

    add more tests

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -25,3 +25,23 @@ it('Should create an item', () => {
 
   expect(createdItem).not.toBeNull();
 });
+
+it('Show Error when press add to do', () => {
+  const {getByText, getByPlaceholderText, queryByText} = render(<App />);
+
+  const textInput = getByPlaceholderText('Write something');
+  const addItemButton = getByText('+');
+
+  const createdItemText = '';
+  const errorMsg = 'Please insert a valid text';
+
+  fireEvent.changeText(textInput, createdItemText);
+  fireEvent.press(addItemButton);
+
+  const createdItem = queryByText(createdItemText);
+
+  const errorMessage = getByText(errorMsg);
+
+  expect(createdItem).toBeNull();
+  expect(errorMessage).not.toBeNull();
+});
