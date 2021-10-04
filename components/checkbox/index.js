06dbb86b43b13898f77d938f1a7cb5db07f4d18e
soroush behmardi <sori.bk@gmail.com>
2021-10-04 15:44:35 -0700

    add new file

---
+++ components/checkbox/index.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+import {TouchableOpacity, Text, StyleSheet, View} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 8,
+  },
+  title: {
+    justifyContent: 'center',
+    marginHorizontal: 16,
+  },
+  green: {
+    color: 'green',
+  },
+});
+
+const Checkbox = ({title}) => {
+  const [checked, setChecked] = useState(false);
+  const onPress = () => {
+    setChecked(!checked);
+  };
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text>{title}</Text>
+        {
+          <Text style={[styles.title, checked && styles.checked]}>
+            {checked ? 'Checked' : 'Not Checked'}
+          </Text>
+        }
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+Checkbox.displayName = 'Checkbox';
+export default Checkbox;
